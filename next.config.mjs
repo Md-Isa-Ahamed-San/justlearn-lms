9316adeca4675a5222bea86c69bedfe17f6202a8
Md-Isa-Ahamed-San <glordisagtav@gmail.com>
2026-02-26 12:37:27 +0600

    trying to fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ['@prisma/client', 'prisma'],
   images: {
     remotePatterns: [
       {
@@ -12,3 +13,4 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
